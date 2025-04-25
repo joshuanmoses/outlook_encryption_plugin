299e8893e2f09c6e6c2fa508a346cb9572f1557d
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-25 15:27:11 -0500

    Create config.py

---
+++ plugin/config.py
@@ -0,0 +1,7 @@
+import os
+
+CONFIG = {
+    "encrypt_by_default": True,
+    "public_key_path": os.path.abspath("keys/public_key.pem"),
+    "private_key_path": os.path.abspath("keys/private_key.pem"),
+}
